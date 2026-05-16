ebfebbaaf32bbdbdaaf079bc3dbdd0c950cafff6
Tamanna Dhang <tamannadhang@gmail.com>
2026-05-16 17:21:37 +0530

    fixed utils folder

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { setToken } from "../tempUtils/auth";
+import { setToken } from "../utils/auth";
 
 function Login() {
   const [email, setEmail] = useState("");
